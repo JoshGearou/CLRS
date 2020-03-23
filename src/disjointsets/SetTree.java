3abf90b3a15895f5abe902dc2228d3f0d06858c2
jgearou <josh.gearou@verizonmedia.com>
2020-03-22 18:26:40 -0700

    added comments to tree representation of disjoint sets

--- src/disjointsets/SetTree.java
+++ src/disjointsets/SetTree.java
@@ -6,18 +6,19 @@ public class SetTree<E> {
     public SetTree() {
 
     }
+
     public SetTree(E val) {
         TreeNode<E> temp = new TreeNode<>(val);
         makeSet(temp);
     }
 
     public TreeNode<E> getRoot() {
-        return root;
+        return root; // The representative element of each disjoint set.
     }
 
     public static class TreeNode<E> {
         E val;
-        int rank;
+        int rank; // An upper bound on the height of the node
         TreeNode<E> parent;
 
         public TreeNode(E val) {
@@ -26,6 +27,11 @@ public class SetTree<E> {
         }
     }
 
+    /**
+     * Creates a set with one element, x
+     * @param x Element to add to empty set
+     * @return {x}
+     */
     public SetTree<E> makeSet(TreeNode<E> x) {
         x.parent = x;
         x.rank = 0;
@@ -33,12 +39,22 @@ public class SetTree<E> {
         return this;
     }
 
-    // assumes x and y are in different sets
+    /**
+     * Combines two disjoint sets into one (note this method assumes the sets are disjoint)
+     * @param x first set
+     * @param y second set
+     * @return new set
+     */
     public SetTree<E> union(TreeNode<E> x, TreeNode<E> y) {
         this.root = link(findSet(x), findSet(y));
         return this;
     }
 
+    /**
+     *
+     * @param x finds the representative element of the set that x belongs to (if it exists)
+     * @return representative element
+     */
     public TreeNode<E> findSet(TreeNode<E> x) {
         if (x.parent != x) {
             x.parent = findSet(x.parent);
@@ -46,6 +62,12 @@ public class SetTree<E> {
         return x.parent;
     }
 
+    /**
+     * Helper method for #union to combine two disjoint sets
+     * @param x first set
+     * @param y second set
+     * @return root of new set
+     */
     public TreeNode<E> link(TreeNode<E> x, TreeNode<E> y) {
         if (x.rank > y.rank) {
             y.parent = x;
@@ -62,6 +84,7 @@ public class SetTree<E> {
     public static void main(String[] args) {
         SetTree<Integer> set1 = new SetTree<>(1);
         SetTree<Integer> set2 = new SetTree<>(2);
-        System.out.println(new SetTree<Integer>().findSet(set1.getRoot()) == new SetTree<Integer>().findSet(set1.getRoot()));
+        new SetTree<Integer>().union(set1.getRoot(), set2.getRoot());
+        System.out.println(new SetTree<Integer>().findSet(set1.getRoot()) == new SetTree<Integer>().findSet(set2.getRoot()));
     }
 }
--- src/graphalgorithms/StronglyConnectedComponents.java
+++ src/graphalgorithms/StronglyConnectedComponents.java
@@ -2,5 +2,5 @@ package graphalgorithms;
 
 public class StronglyConnectedComponents {
 
-    
+
 }
