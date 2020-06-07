8a7762a993a729bfe0c858acb9a7553c77077f9f
jgearou <josh.gearou@verizonmedia.com>
2020-06-07 12:57:05 -0500

    update quicksort

--- src/sorting/Quicksort.java
+++ src/sorting/Quicksort.java
@@ -1,12 +1,13 @@
 package sorting;
 
-import java.util.Random;
-
 public class Quicksort {
+
+    private void quicksort(int[] data) {
+        quicksort(data, 0, data.length-1);
+    }
     private void quicksort(int[] data, int s, int e) {
         if (s < e) {
             int q = partition(data, s, e);
-            System.out.println(q);
             quicksort(data, s, q-1);
             quicksort(data, q+1, e);
         }
@@ -21,8 +22,9 @@ public class Quicksort {
                 swap(data, i, j);
             }
         }
-        swap(data, i+1, e);
-        return i+1;
+        i++;
+        swap(data, i, e);
+        return i;
     }
 
     private int randomPartition(int[] data, int s, int e) {
@@ -35,14 +37,5 @@ public class Quicksort {
         int temp = data[i];
         data[i] = data[j];
         data[j] = temp;
-    }
-
-    public static void main (String[] args) {
-        int[] data = {5,3,2,6,8,10,4};
-        new Quicksort().quicksort(data, 0, data.length-1);
-//        for (int i: data) {
-//            System.out.println(i);
-//        }
-
     }
 }
