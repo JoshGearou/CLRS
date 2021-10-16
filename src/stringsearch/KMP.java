3799d049f56b99b8dc593050bfac75024f89b0e0
JoshGearou <joshgearou@gmail.com>
2021-10-15 22:12:33 -0400

    shorten KMP

--- src/stringsearch/KMP.java
+++ src/stringsearch/KMP.java
@@ -5,55 +5,42 @@ import java.util.List;
 
 public class KMP {
 
-    public List<Integer> KMPSearch(String str, String pattern) {
-        List<Integer> indices = new ArrayList<>();
-        if (pattern.isEmpty()) {
-            return indices;
-        }
+    public List<Integer> KMPSearch(String text, String pattern) {
         int[] lps = createLps(pattern);
+        List<Integer> indices = new ArrayList<>();
+        int n = text.length();
+        int m = pattern.length();
         int i = 0;
         int j = 0;
-        while (i < str.length() && j < lps.length) {
-            if (str.charAt(i) == pattern.charAt(j)) {
-                i++;
-                j++;
-            } else {
-                if (j == 0) {
-                    i++;
-                } else {
-                    j = lps[j - 1];
-                }
+        while (i < n) {
+            while (j >= 0 && text.charAt(i) != pattern.charAt(j)) {
+                j = lps[j];
             }
-
-            if (j == lps.length) {
-                indices.add(i - lps.length);
-                j = lps[j - 1];
-
+            i++;
+            j++;
+            if (j == m) {
+                indices.add(i - j);
+                j = lps[j];
             }
         }
         return indices;
     }
 
     public int[] createLps(String pattern) {
-        char[] pat = pattern.toCharArray();
-        int[] lps = new int[pat.length];
+        int m = pattern.length();
+        int[] lps = new int[m+1];
+        int i = 0;
+        int j = -1;
+        lps[0] = -1;
 
-        int i = 1;
-        int len = 0;
-        while (i < pat.length) {
-            if (pat[i] == pat[len]) {
-                len++;
-                lps[i] = len;
-                i++;
-            } else {
-                if (len == 0) {
-                    lps[i] = 0;
-                    i++;
-                } else {
-                    len = lps[len - 1];
-                }
+        while (i < m) {
+            while (j >= 0 && pattern.charAt(i) != pattern.charAt(j)) {
+                j = lps[j];
             }
+            i++;
+            j++;
+            lps[i] = j;
         }
         return lps;
-    }
+    } 
 }
